d6ce5c2f22e4db9d370de2e995c1f75455edcd0b
Yogi <yoginth@hey.com>
2021-11-16 09:10:03 +0000

    feat: add getENS to warmup

--- scripts/warmup.sh
+++ scripts/warmup.sh
@@ -7,6 +7,7 @@ curl https://devparty.io/api/auth/spotify/callback?warmup=true
 curl https://devparty.io/api/auth/getNonce?warmup=true
 curl https://devparty.io/api/masquerade/test?warmup=true
 curl https://devparty.io/api/utils/getIssue?warmup=true
+curl https://devparty.io/api/utils/getENS?warmup=true
 curl https://devparty.io/api/export?warmup=true
 curl https://devparty.io/api/oembed?warmup=true
 curl https://devparty.io/api/logout?warmup=true
