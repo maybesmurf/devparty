b670a0477a999da09e296635a5f3700dd459b3ba
Yogi <yoginth@hey.com>
2021-11-17 05:38:21 +0000

    chore: add apple-touch-icon and closes #223

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -23,6 +23,11 @@ class DevpartyDocument extends Document<Props> {
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <link rel="icon" href="/favicon.svg" />
           <link
+            rel="apple-touch-icon"
+            sizes="192x192"
+            href="https://assets.devparty.io/images/icons/apple-touch-icon.png"
+          />
+          <link
             href="https://assets.devparty.io/css/font.css"
             rel="stylesheet"
           />
