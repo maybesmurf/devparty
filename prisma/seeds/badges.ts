16d68563943d1b2832f89f71d09274b216609f5b
Yogi <yoginth@hey.com>
2021-11-18 11:33:02 +0000

    fead: add badges seed

---
+++ prisma/seeds/badges.ts
@@ -0,0 +1,48 @@
+export const badgeData = [
+  {
+    name: 'One Year Club',
+    image: 'https://i.ibb.co/4Z406dF/1-year-badge.png',
+    description:
+      'This badge celebrates the longevity of those who have been a registered member of the Devparty for at least one year.'
+  },
+  {
+    name: 'Two Year Club',
+    image: 'https://i.ibb.co/ZfP44XK/2year-badge.png',
+    description:
+      'This badge celebrates the longevity of those who have been a registered member of the Devparty for at least two years.'
+  },
+  {
+    name: 'Beloved Comment',
+    image: 'https://i.ibb.co/DKDhddb/beloved-comment-badge-final.png',
+    description:
+      'Awarded for making a comment which the community loved, as voted on with heart (❤️) reactions.'
+  },
+  {
+    name: '4 Week Streak',
+    image: 'https://i.ibb.co/XLXJspL/4-Week-Streak-badge.png',
+    description: 'Awarded for consistency of writing.'
+  },
+  {
+    name: '8 Week Streak',
+    image: 'https://i.ibb.co/pwKwBRm/8-week-streak-Shadow.png',
+    description:
+      'For publishing at least one post per week for eight consecutive weeks. The streak continues!'
+  },
+  {
+    name: '16 Week Streak',
+    image: 'https://i.ibb.co/93jYFV3/16-week-badge.png',
+    description:
+      'Awarded for outstanding consistency and commitment to the community'
+  },
+  {
+    name: 'Vue Badge',
+    image: 'https://i.ibb.co/VQ4PJ6M/vue-sticker.png',
+    description: 'Awarded to the top Vue author each week'
+  },
+  {
+    name: 'Devparty Contributor',
+    image: 'https://i.ibb.co/cw2BCbv/contributor-badge.png',
+    description:
+      'For members who have contributed code or technical docs/guidelines to the Devparty open source project.'
+  }
+]
