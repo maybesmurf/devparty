033f94eba95ebd74e5c7e4b7f6ee0988dc9cea73
Yogi <yoginth@hey.com>
2021-11-18 19:37:18 +0530

    feat: add service comp

--- src/components/StaffTools/index.tsx
+++ src/components/StaffTools/index.tsx
@@ -12,10 +12,12 @@ import {
   CubeIcon,
   DatabaseIcon,
   ExternalLinkIcon,
+  GlobeIcon,
   HashtagIcon,
   HeartIcon,
   IdentificationIcon,
   LoginIcon,
+  ServerIcon,
   ShieldExclamationIcon,
   UserIcon,
   UsersIcon
@@ -52,6 +54,24 @@ const Stat = ({ icon, count, name }: StatProps) => (
     {icon}
     <div>
       <span className="font-bold">{humanize(count)}</span> {name}
+    </div>
+  </div>
+)
+
+interface ServiceProps {
+  icon: React.ReactNode
+  url: string
+  name: string
+}
+
+const Service = ({ icon, url, name }: ServiceProps) => (
+  <div className="flex items-center space-x-1.5">
+    {icon}
+    <div className="flex items-center space-x-1">
+      <a href={url} className="font-bold" target="_blank" rel="noreferrer">
+        {name}
+      </a>
+      <ExternalLinkIcon className="h-3 w-3" />
     </div>
   </div>
 )
@@ -129,34 +149,26 @@ const StaffToolsDashboard: React.FC = () => {
             <div className="border-b dark:border-gray-800 my-3" />
             <div className="text-xl font-bold mb-1.5">Services</div>
             <div className="text-gray-700 dark:text-gray-300 space-y-1">
-              <div className="flex items-center space-x-1.5">
-                <DatabaseIcon className="h-4 w-4" />
-                <div className="flex items-center space-x-1">
-                  <a
-                    href="https://app.planetscale.com/yo/devparty"
-                    className="font-bold"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    PlanetScale
-                  </a>
-                  <ExternalLinkIcon className="h-3 w-3" />
-                </div>
-              </div>
-              <div className="flex items-center space-x-1.5">
-                <ChipIcon className="h-4 w-4" />
-                <div className="flex items-center space-x-1">
-                  <a
-                    href="https://console.upstash.com/redis/21cb559e-de14-44b9-a67f-05c67f2c8376"
-                    className="font-bold"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Upstash
-                  </a>
-                  <ExternalLinkIcon className="h-3 w-3" />
-                </div>
-              </div>
+              <Service
+                icon={<ServerIcon className="h-4 w-4" />}
+                url="https://vercel.com/devparty/devparty?utm_source=Devparty&utm_campaign=oss"
+                name="Vercel"
+              />
+              <Service
+                icon={<DatabaseIcon className="h-4 w-4" />}
+                url="https://app.planetscale.com/yo/devparty"
+                name="PlanetScale"
+              />
+              <Service
+                icon={<ChipIcon className="h-4 w-4" />}
+                url="https://console.upstash.com/redis/21cb559e-de14-44b9-a67f-05c67f2c8376"
+                name="Upstash"
+              />
+              <Service
+                icon={<GlobeIcon className="h-4 w-4" />}
+                url="https://graphcdn.io/dashboard/yogi"
+                name="GraphCDN"
+              />
             </div>
           </CardBody>
         </Card>
