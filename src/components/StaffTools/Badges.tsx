24dbc1521e4eed9db932981a714907e84fa12e6e
Yogi <yoginth@hey.com>
2021-11-18 19:17:34 +0530

    feat: add title and button

--- src/components/StaffTools/Badges.tsx
+++ src/components/StaffTools/Badges.tsx
@@ -1,11 +1,13 @@
 import { gql, useQuery } from '@apollo/client'
 import { GridItemEight, GridItemFour, GridLayout } from '@components/GridLayout'
+import { Button } from '@components/UI/Button'
 import { Card, CardBody } from '@components/UI/Card'
 import { ErrorMessage } from '@components/UI/ErrorMessage'
 import { PageLoading } from '@components/UI/PageLoading'
 import { Spinner } from '@components/UI/Spinner'
 import { imagekitURL } from '@components/utils/imagekitURL'
 import { GetStaffBadgesQuery } from '@graphql/types.generated'
+import { PlusCircleIcon } from '@heroicons/react/outline'
 import React from 'react'
 import useInView from 'react-cool-inview'
 
@@ -76,32 +78,40 @@ const StaffToolsBadges: React.FC = () => {
       </GridItemFour>
       <GridItemEight>
         <Card>
-          <CardBody className="divide-y">
-            <ErrorMessage title="Failed to load badges" error={error} />
-            {badges?.map((badge: any) => (
-              <div
-                key={badge?.id}
-                className="flex justify-between items-center py-5"
-              >
-                <div className="flex space-x-4 items-center">
-                  <img
-                    src={imagekitURL(badge?.image as string, 100, 100)}
-                    className="w-24"
-                    alt={badge?.name}
-                  />
-                  <div>
-                    <div className="flex items-center gap-1.5">
-                      {badge?.name}
-                    </div>
-                    {badge?.description && (
-                      <div className="mt-2 text-gray-600 dark:text-gray-300">
-                        {badge?.description}
+          <CardBody>
+            <div className="flex items-center justify-between">
+              <div className="text-xl font-bold">Badges</div>
+              <Button icon={<PlusCircleIcon className="h-5 w-5" />}>
+                New Badge
+              </Button>
+            </div>
+            <div className="divide-y">
+              <ErrorMessage title="Failed to load badges" error={error} />
+              {badges?.map((badge: any) => (
+                <div
+                  key={badge?.id}
+                  className="flex justify-between items-center py-5"
+                >
+                  <div className="flex space-x-4 items-center">
+                    <img
+                      src={imagekitURL(badge?.image as string, 100, 100)}
+                      className="w-24"
+                      alt={badge?.name}
+                    />
+                    <div>
+                      <div className="flex items-center gap-1.5">
+                        {badge?.name}
                       </div>
-                    )}
+                      {badge?.description && (
+                        <div className="mt-2 text-gray-600 dark:text-gray-300">
+                          {badge?.description}
+                        </div>
+                      )}
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </CardBody>
         </Card>
         {pageInfo?.hasNextPage && (
