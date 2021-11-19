9be36de463dee4d82b9fd9e8a380c8419cd2ab04
Yogi <yoginth@hey.com>
2021-11-19 14:11:39 +0000

    fix: eslint issues, thanks @jsjoeio

--- src/components/User/Streak.tsx
+++ src/components/User/Streak.tsx
@@ -1,36 +1,36 @@
 import { Card, CardBody } from '@components/UI/Card'
 import { motion } from 'framer-motion'
-import { useEffect, useState } from 'react'
 import { STATIC_ASSETS } from 'src/constants'
 import { useStreak } from 'use-streak'
 
 const Day = ({ day }: { day: number }) => (
   <motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
-    <img className="h-10" src={`${STATIC_ASSETS}/streak/${day}.png`} />
+    <img
+      className="h-10"
+      src={`${STATIC_ASSETS}/streak/${day}.png`}
+      alt={`Day ${day}`}
+    />
   </motion.button>
 )
 
 const Streak = () => {
   const today = new Date()
-  const [streak, setStreak] = useState<number>()
+  const streak =
+    // eslint-disable-next-line
+    typeof window !== 'undefined' ? useStreak(localStorage, today) : undefined
 
-  useEffect(() => {
-    const streak = useStreak(localStorage, today)
-    setStreak(streak.currentCount)
-  }, [])
-
-  if ((streak as number) > 5) return null
+  if ((streak?.currentCount as number) > 5) return null
 
   return (
     <Card className="mb-4">
       <CardBody className="space-y-2 flex items-center justify-between">
         <div>
           <div>You're on a</div>
-          <div className="text-xl">{streak} day streak ⚡️</div>
+          <div className="text-xl">
+            {streak && streak.currentCount} day streak ⚡️
+          </div>
         </div>
-        <div>
-          <Day day={streak as number} />
-        </div>
+        <div>{streak && <Day day={streak.currentCount} />}</div>
       </CardBody>
     </Card>
   )
