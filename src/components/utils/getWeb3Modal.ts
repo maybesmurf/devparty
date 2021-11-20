fd5db364d821ff04ba90fd6b5981e556fb8ece4c
Yogi <yoginth@hey.com>
2021-11-20 14:11:01 +0000

    chore: fix wallet login

--- src/components/utils/getWeb3Modal.ts
+++ src/components/utils/getWeb3Modal.ts
@@ -1,6 +1,5 @@
 import WalletConnectProvider from '@walletconnect/web3-provider'
-import { INFURA_ID, STATIC_ASSETS } from 'src/constants'
-import WalletLink from 'walletlink'
+import { INFURA_ID } from 'src/constants'
 import Web3Modal from 'web3modal'
 
 const getWeb3Modal = (theme: string) => {
@@ -8,26 +7,6 @@ const getWeb3Modal = (theme: string) => {
     walletconnect: {
       package: WalletConnectProvider,
       options: { infuraId: INFURA_ID }
-    },
-    'custom-walletlink': {
-      display: {
-        logo: `${STATIC_ASSETS}/brands/coinbase.svg`,
-        name: 'Coinbase',
-        description: 'Use the Coinbase Wallet app on your mobile device'
-      },
-      options: {
-        appName: 'Devparty',
-        networkUrl: `https://mainnet.infura.io/v3/${INFURA_ID}`
-        // chainId: 1
-      },
-      package: WalletLink,
-      connector: async (_: any, options: any) => {
-        const { appName, networkUrl, chainId } = options
-        const walletLink = new WalletLink({ appName })
-        const provider = walletLink.makeWeb3Provider(networkUrl, chainId)
-        await provider.enable()
-        return provider
-      }
     }
   }
 
