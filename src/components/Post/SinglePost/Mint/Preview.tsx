7231538eefc6e9a85d0b32e53dc9208d0cbee7da
Yogi <yoginth@hey.com>
2021-11-22 19:07:09 +0530

    feat: add preview

--- src/components/Post/SinglePost/Mint/Preview.tsx
+++ src/components/Post/SinglePost/Mint/Preview.tsx
@@ -1,18 +1,31 @@
+import { Checkbox } from '@components/UI/Checkbox'
 import { Post } from '@graphql/types.generated'
+import { useState } from 'react'
 
 interface Props {
   post: Post
 }
 
 const MintPreview: React.FC<Props> = ({ post }) => {
+  const [theme, setTheme] = useState<'dark' | 'light'>('light')
+
   return (
-    <div className="relative group w-60 h-60">
-      <div className="absolute -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
-      <img
-        className="relative rounded-lg border bg-gray-200 dark:bg-gray-700 ring-gray-50 dark:ring-black"
-        src={`https://nft.devparty.io/${post?.body}?avatar=${post?.user?.profile?.avatar}`}
-        alt={post?.body}
-      />
+    <div className="space-y-5">
+      <div className="relative group w-60 h-60">
+        <div className="absolute -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
+        <img
+          className="relative rounded-lg"
+          src={`https://nft.devparty.io/${post?.body}?avatar=${post?.user?.profile?.avatar}&theme=${theme}`}
+          alt={post?.body}
+        />
+      </div>
+      <div className="flex items-center space-x-3">
+        <Checkbox
+          id="darkBg"
+          onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+        />
+        <label htmlFor="darkBg">Use Dark Background</label>
+      </div>
     </div>
   )
 }
