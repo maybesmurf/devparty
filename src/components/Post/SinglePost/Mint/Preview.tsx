f4ac29de26ebcd8f793a67d18a3f1eb1efb73038
Yogi <yoginth@hey.com>
2021-11-22 18:46:39 +0530

    chore: add gradient preview

--- src/components/Post/SinglePost/Mint/Preview.tsx
+++ src/components/Post/SinglePost/Mint/Preview.tsx
@@ -6,12 +6,15 @@ interface Props {
 
 const MintPreview: React.FC<Props> = ({ post }) => {
   return (
-    <div>
-      <img
-        className="w-72 rounded-lg border-2"
-        src={`https://nft.devparty.io/${post?.body}`}
-        alt={post?.body}
-      />
+    <div className="grid gap-8 items-start justify-center">
+      <div className="relative group w-60 h-60">
+        <div className="absolute -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
+        <img
+          className="relative rounded-lg border bg-gray-200 dark:bg-gray-700 ring-gray-50 dark:ring-black"
+          src={`https://nft.devparty.io/${post?.body}?avatar=${post?.user?.profile?.avatar}`}
+          alt={post?.body}
+        />
+      </div>
     </div>
   )
 }
