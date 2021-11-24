d4b39a08888ac4e0d13e87dbff1fee13bdfdae1f
Yogi <yoginth@hey.com>
2021-11-24 11:52:47 +0530

    feat: show eth

--- src/components/User/Tips/SingleTier.tsx
+++ src/components/User/Tips/SingleTier.tsx
@@ -16,7 +16,10 @@ const SingleTier: React.FC<Props> = ({ tier, address }) => {
   return (
     <div className="py-5 space-y-3 flex items-start justify-between space-x-5">
       <div className="space-y-3">
-        <div className="text-lg font-bold">{tier?.amount} Ξ one time</div>
+        <div>
+          <div className="text-lg font-bold">{tier?.amount} MATIC</div>
+          <div className="text-gray-500 font-light text-sm">5 ETH</div>
+        </div>
         <div className="text-lg font-bold">{tier?.name}</div>
         <div className="prose dark:prose-dark">
           <Markdown options={{ wrapper: 'article' }}>
