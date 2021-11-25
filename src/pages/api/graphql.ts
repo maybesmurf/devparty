d82e01ddc69cd78fc395bb8f2e4801b3e90fa155
Yogi <yoginth@hey.com>
2021-11-25 18:45:26 +0530

    chore: remove inbuild depthLimit

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -2,7 +2,6 @@ import { Context, createGraphQLContext } from '@graphql/builder'
 import schema from '@graphql/index'
 import { resolveSession } from '@utils/sessions'
 import { ExecutionResult, GraphQLError } from 'graphql'
-import depthLimit from 'graphql-depth-limit'
 import {
   getGraphQLParameters,
   processRequest,
@@ -75,7 +74,6 @@ const handler: NextApiHandler = async (req, res) => {
         variables,
         request,
         schema,
-        validationRules: [depthLimit(15)],
         contextFactory: () => createGraphQLContext(req, res, session)
       })
 
