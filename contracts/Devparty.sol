94509d933680d95f6efd455cd8e7ad2acb26c4fe
Yogi <yoginth@hey.com>
2021-11-29 03:06:06 +0000

    feat: add contracts

---
+++ contracts/Devparty.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Devparty is Ownable, ERC1155Supply, ReentrancyGuard {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    mapping(uint256 => string) private _tokenURIs;
+
+    constructor() ERC1155("") {}
+
+    function contractURI() public pure returns (string memory) {
+        return
+            "https://bafybeiglzzl2g3y362yyzbnutl3dku2q43oybafbnqaycmbvikqql67usm.ipfs.infura-ipfs.io";
+    }
+
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI)
+        internal
+        virtual
+    {
+        _tokenURIs[tokenId] = _tokenURI;
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        returns (string memory)
+    {
+        require(exists(tokenId), "URI query for nonexistent token");
+        string memory _tokenURI = _tokenURIs[tokenId];
+
+        return _tokenURI;
+    }
+
+    /**
+     * Mint + Issue Token
+     *
+     * @param recipient - Token will be issued to recipient
+     * @param amount - amount of tokens to mint
+     * @param uri - IPFS URL
+     */
+    function issueToken(
+        address recipient,
+        uint256 amount,
+        string memory uri
+    ) public nonReentrant returns (uint256) {
+        require(amount != 0, "Quantity should be positive");
+
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId, amount, "");
+        _setTokenURI(newItemId, uri);
+
+        return newItemId;
+    }
+
+    /**
+     * Tip a user
+     *
+     * @param recipient - Recipient address to be tipped
+     */
+    function tipUser(address payable recipient) external payable nonReentrant {
+        (bool success, ) = recipient.call{value: msg.value}("");
+        require(success, "Transfer failed.");
+    }
+}
---
+++ test/contract-test.js
@@ -0,0 +1,31 @@
+const { expect } = require('@playwright/test')
+
+describe('Devparty Contract', function () {
+  it('should issue token (issueToken)', async function () {
+    const Devparty = await ethers.getContractFactory('Devparty')
+    const devparty = await Devparty.deploy()
+    await devparty.deployed()
+    const signerAddress = '3A5bd1E37b099aE3386D13947b6a90d97675e5e3'
+    const token = await devparty.issueToken(
+      `0x${signerAddress}`,
+      69,
+      'https://bafybeiewuo3rq2glwgg7f2n743svfvgw5zaiyoxuwqqc54ogy5hobmln4i.ipfs.infura-ipfs.io/'
+    )
+
+    expect(token.data.includes(signerAddress.toLowerCase())).toBe(true)
+  })
+
+  it('should tip user (tipUser)', async function () {
+    const Devparty = await ethers.getContractFactory('Devparty')
+    const devparty = await Devparty.deploy()
+    await devparty.deployed()
+    const token = await devparty.tipUser(
+      '0xd3B307753097430FaEdFdb89809610bF8e8f3203',
+      {
+        value: ethers.utils.parseEther('1').toString(10)
+      }
+    )
+
+    console.log(token)
+  })
+})
