a12574f5870c515f2ce27e88bdc6681b62b3a1a3
Yogi <yoginth@hey.com>
2021-11-29 04:57:49 +0000

    gp

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,11 @@
 const hre = require('hardhat')
+const fs = require('fs')
 
 async function main() {
   const Devparty = await hre.ethers.getContractFactory('Devparty')
   const devparty = await Devparty.deploy()
   await devparty.deployed()
+  fs.appendFileSync('.env', `DEV_CONTRACT_ADDRESS="${devparty.address}"\n`)
   console.log('Devparty deployed to:', devparty.address)
 }
 
