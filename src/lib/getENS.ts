ff94058a7b58de2c58ce737686cf8cd8ee7a490d
Yogi <me@yogi.codes>
2021-11-30 11:28:07 +0530

    feat: add dynamic rpc

--- src/lib/getENS.ts
+++ src/lib/getENS.ts
@@ -1,10 +1,11 @@
 // @ts-ignore
 import ENS, { getEnsAddress } from '@ensdomains/ensjs'
 import { ethers } from 'ethers'
+import { IS_PRODUCTION, MAINET_RPC, RINKEBY_RPC } from 'src/constants'
 
 const getENS = async (address: string): Promise<string> => {
   const provider = new ethers.providers.JsonRpcProvider(
-    'https://cloudflare-eth.com'
+    IS_PRODUCTION ? MAINET_RPC : RINKEBY_RPC
   )
   const ensClient = new ENS({ provider, ensAddress: getEnsAddress('1') })
   const ens = await ensClient.getName(address)
