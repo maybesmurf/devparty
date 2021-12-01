9ffe23b5ec0ac5e8843237c49bfa0a720a97173d
Yogi <me@yogi.codes>
2021-12-01 20:01:43 +0530

    chore: fix pricing

--- src/components/User/Tips/Tip.tsx
+++ src/components/User/Tips/Tip.tsx
@@ -80,7 +80,7 @@ const Tip: React.FC<Props> = ({ tier, address, eth }) => {
           .parseEther(
             ['matic', 'maticmum'].includes(network)
               ? tier?.amount?.toString()
-              : eth.toString()
+              : eth.toFixed(5)
           )
           // @ts-ignore
           .toString(10)
@@ -102,6 +102,7 @@ const Tip: React.FC<Props> = ({ tier, address, eth }) => {
       setProgressStatus('COMPLETED')
       toast.success('Sponsor Transaction completed!')
     } catch (error: any) {
+      console.log(error)
       setProgressStatus('NOTSTARTED')
       setTxURL('')
       setError(
