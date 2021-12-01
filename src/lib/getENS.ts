ea99bcaad8e9680bddfdcb29ba7cc9a44417d61f
Yogi <me@yogi.codes>
2021-12-01 20:00:01 +0530

    feat: use chainlink for prize

--- src/lib/getENS.ts
+++ src/lib/getENS.ts
@@ -1,11 +1,11 @@
 // @ts-ignore
 import ENS, { getEnsAddress } from '@ensdomains/ensjs'
 import { ethers } from 'ethers'
-import { IS_PRODUCTION, MAINET_RPC, RINKEBY_RPC } from 'src/constants'
+import { IS_PRODUCTION, MAINNET_RPC, RINKEBY_RPC } from 'src/constants'
 
 const getENS = async (address: string): Promise<string> => {
   const provider = new ethers.providers.JsonRpcProvider(
-    IS_PRODUCTION ? MAINET_RPC : RINKEBY_RPC
+    IS_PRODUCTION ? MAINNET_RPC : RINKEBY_RPC
   )
   const ensClient = new ENS({ provider, ensAddress: getEnsAddress('1') })
   const ens = await ensClient.getName(address)
