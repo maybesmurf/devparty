e48299a715ee8920f4c3e493bbdd3948b70e889a
Yogi <yoginth@hey.com>
2021-12-03 03:33:17 +0000

    chore: typo

--- src/components/Pro/index.tsx
+++ src/components/Pro/index.tsx
@@ -10,7 +10,7 @@ const Pro: React.FC = () => {
     <div className="bg-gray-100 dark:bg-gray-900">
       <section className="px-2 pt-20 dark:text-white md:px-0">
         <div className="container items-center max-w-6xl px-5 mx-auto space-y-6 text-center">
-          <h1 className="text-4xl font-extrabold tracking-tight text-left sm:text-5xl md:text-6xl md:text-center">
+          <div className="text-4xl font-extrabold tracking-tight text-left sm:text-5xl md:text-6xl md:text-center">
             <span className="block">
               pick a plan that's{' '}
               <span className="block mt-1 text-purple-500 lg:inline lg:mt-0">
@@ -18,7 +18,7 @@ const Pro: React.FC = () => {
               </span>{' '}
               for you!
             </span>
-          </h1>
+          </div>
           <p className="w-full mx-auto text-base text-left text-gray-500 dark:text-gray-400 md:max-w-md sm:text-lg lg:text-2xl md:text-center">
             Pay for the features that you need.
           </p>
@@ -29,49 +29,44 @@ const Pro: React.FC = () => {
               {
                 validity: '1 Month',
                 amount: 0.05,
-                prefered: false,
+                preferred: false,
                 bgImage: '1'
               },
               {
                 validity: '6 Month',
                 amount: 0.1,
-                prefered: true,
+                preferred: true,
                 bgImage: '2'
               },
-              { validity: '1 Year', amount: 0.2, prefered: false, bgImage: '1' }
-            ].map(({ validity, amount, prefered, bgImage }, idx) => (
+              {
+                validity: '1 Year',
+                amount: 0.2,
+                preferred: false,
+                bgImage: '1'
+              }
+            ].map(({ validity, amount, preferred, bgImage }, idx) => (
               <div
                 key={idx}
                 className={clsx(
                   'relative mx-auto flex flex-col max-w-xs w-full items-center p-2 overflow-hidden dark:bg-gray-800 bg-white rounded-lg lg:items-start border',
-                  { 'transform md:scale-110': prefered }
+                  { 'transform md:scale-110': preferred }
                 )}
               >
-                {prefered && (
-                  <img
-                    className="absolute top-0 right-0"
-                    src="/eth.svg"
-                    alt=""
-                    width={50}
-                    height={50}
-                    draggable={false}
-                  />
-                )}
                 <div
                   style={{
                     backgroundImage: `url(https://assets.devparty.io/images/patterns/${bgImage}.svg)`,
                     backgroundSize: '200%'
                   }}
-                  className="flex object-cover shadow bg-indigo-300 rounded-t-lg p-6 flex-row justify-center w-full items-center"
+                  className="flex object-cover shadow bg-brand-300 rounded-t-lg p-6 flex-row justify-center w-full items-center"
                 >
-                  <h1 className="text-lg text-center text-black leading-snug text-light md:text-2xl">
+                  <div className="text-lg text-center text-black leading-snug text-light md:text-2xl">
                     {validity}
-                  </h1>
+                  </div>
                 </div>
                 <div className="text-left p-4 space-y-5 dark:bg-black w-full text-gray-400">
-                  <h1 className="text-center p-5 text-5xl text-black dark:text-white font-bold">
+                  <div className="text-center p-5 text-5xl text-black dark:text-white font-bold">
                     <span className="text-3xl">Ξ</span> {amount}
-                  </h1>
+                  </div>
                   <ul className="!list-disc !list-inside space-y-2">
                     <li>Lorem Ipsum is simply Ipsum is simply Ipsum simply</li>
                     <li>Lorem Ipsum is simply Ipsum is simply Ipsum simply</li>
@@ -91,9 +86,9 @@ const Pro: React.FC = () => {
 
       <section className="py-20 bg-gray-50 dark:bg-gray-900">
         <div className="container items-center max-w-6xl px-10 mx-auto sm:px-20 md:px-32 lg:px-16">
-          <h1 className="text-2xl font-extrabold tracking-tight text-left sm:text-3xl md:text-4xl md:text-center">
+          <div className="text-2xl font-extrabold tracking-tight text-left sm:text-3xl md:text-4xl md:text-center">
             what people frequently ask about us?
-          </h1>
+          </div>
           <div className="flex flex-wrap items-center py-16">
             <div className="w-full max-w-xl p-2 mx-auto rounded-lg border">
               <Disclosure>
