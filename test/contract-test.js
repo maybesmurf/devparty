5db5cf092a2955575ae5389b0d219d4e237ebd41
Yogi <me@yogi.codes>
2021-12-04 19:11:21 +0530

    fix: tests

--- test/contract-test.js
+++ test/contract-test.js
@@ -14,27 +14,4 @@ describe('Devparty Contract', function () {
 
     expect(token.data.includes(signerAddress.toLowerCase())).toBe(true)
   })
-
-  it('should tip user (tipUser)', async function () {
-    const Devparty = await ethers.getContractFactory('Devparty')
-    const devparty = await Devparty.deploy()
-    await devparty.deployed()
-    const token = await devparty.tipUser(
-      '0xd3B307753097430FaEdFdb89809610bF8e8f3203',
-      {
-        value: ethers.utils.parseEther('1').toString(10)
-      }
-    )
-
-    console.log(token)
-  })
-
-  it('should subscribe to pro (subscribeToPro)', async function () {
-    const Devparty = await ethers.getContractFactory('Devparty')
-    const devparty = await Devparty.deploy()
-    await devparty.deployed()
-    const token = await devparty.subscribeToPro()
-
-    console.log(token)
-  })
 })
