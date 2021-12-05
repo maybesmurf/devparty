d6bb806ec2f45402f52b85df158460de71b2b2f3
Yogi <me@yogi.codes>
2021-12-05 12:11:08 +0530

    feat: add followers modal

--- src/components/User/Followerings.tsx
+++ src/components/User/Followerings.tsx
@@ -3,6 +3,7 @@ import { User } from '@graphql/types.generated'
 import { humanize } from '@lib/humanize'
 import React, { useState } from 'react'
 
+import Followers from './Followers'
 import Following from './Following'
 
 interface Props {
@@ -11,6 +12,7 @@ interface Props {
 
 const Followerings: React.FC<Props> = ({ user }) => {
   const [showFollowingModal, setShowFollowingModal] = useState<boolean>(false)
+  const [showFollowersModal, setShowFollowersModal] = useState<boolean>(false)
 
   return (
     <div className="flex gap-5">
@@ -21,7 +23,10 @@ const Followerings: React.FC<Props> = ({ user }) => {
         <div className="text-xl">{humanize(user?.following?.totalCount)}</div>
         <div className="text-gray-500">Following</div>
       </div>
-      <div>
+      <div
+        className="cursor-pointer"
+        onClick={() => setShowFollowersModal(!showFollowersModal)}
+      >
         <div className="text-xl">{humanize(user?.followers?.totalCount)}</div>
         <div className="text-gray-500">Followers</div>
       </div>
@@ -32,6 +37,14 @@ const Followerings: React.FC<Props> = ({ user }) => {
         onClose={() => setShowFollowingModal(!showFollowingModal)}
       >
         <Following />
+      </Modal>
+      <Modal
+        title="Followers"
+        size="lg"
+        show={showFollowersModal}
+        onClose={() => setShowFollowersModal(!showFollowersModal)}
+      >
+        <Followers />
       </Modal>
     </div>
   )
