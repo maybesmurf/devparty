dae1944e3c59a0fdbb7b8df1512851de6f0f81c1
Yogi <me@yogi.codes>
2021-12-08 19:42:37 +0530

    fix

--- src/lib/getENS.ts
+++ src/lib/getENS.ts
@@ -1,6 +1,5 @@
-// @ts-ignore
-
 const getENS = async (address: string): Promise<string> => {
+  console.log(address)
   const response = await fetch(
     `https://deep-index.moralis.io/api/v2/resolve/${address}/reverse`,
     { headers: { 'X-API-Key': process.env.MORALIS_API_KEY as string } }
